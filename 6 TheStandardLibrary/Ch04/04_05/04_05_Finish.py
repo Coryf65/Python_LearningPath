b5c6dc1bc765a0f36b23d0d835b28839bd44ccd1
CoryF <coryf65@gmail.com>
2020-07-12 22:30:34 -0500

    Use the Google Books API to pull in some JSON data

--- 6 TheStandardLibrary/Ch04/04_05/04_05_Finish.py
+++ 6 TheStandardLibrary/Ch04/04_05/04_05_Finish.py
@@ -2,7 +2,11 @@
 
 from html.parser import HTMLParser
 
-class HTMLParser(HTMLParser):
+# setting my file and pathing
+my_path = 'c:/Users/Cory/Documents/_Code/Python_LearningPath/6 TheStandardLibrary/Ch04/04_05/'
+html_file = 'sampleHTML.html'
+
+class Parser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         print("Start tag: ", tag)
         for attr in attrs:
@@ -14,16 +18,16 @@ class HTMLParser(HTMLParser):
     def handle_data(self, data):
         print("Data: ", data)
 
-parser = HTMLParser()
+parser = Parser()
 parser.feed("<html><head><title>Coder</title></head><body><h1><!--hi-->I am a coder</h1></body></html>")
-print()
+print() # prints an empty line
 
 input = input("Put in HTML Code")
 parser.feed(input)
-print()
+print() # prints an empty line
 
-htmlFile = open("sampleHTML.html", "r")
-s = ""
+htmlFile = open(my_path + html_file, "r")
+s = "" # this holds our each line of the file to feed for the parser
 for line in htmlFile:
     s += line
 parser.feed(s)
--- 6 TheStandardLibrary/Ch04/04_07/04_07_Finish.py
+++ 6 TheStandardLibrary/Ch04/04_07/04_07_Finish.py
@@ -6,14 +6,17 @@ import urllib.request
 import json
 import textwrap
 
-with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:1101904224") as f:
-    text = f.read()
-    decodedtext = text.decode('utf-8')
-    print(textwrap.fill(decodedtext, width=50))
+# using google books API, we are pulling a GET request
+with urllib.request.urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:1101904224") as f: # if this runs we create it as a var f
+    text = f.read() 
+    decodedtext = text.decode('utf-8') # this is decoded so we can print it out
+    print(textwrap.fill(decodedtext, width=50)) # some formatting
 
-print()
+print() # new line
 
+# this API returns a JSON object, Going to the link we can see this
 obj = json.loads(decodedtext)
+# if we look at the JSON return info we can see a Key, 'kind'
 print(obj['kind'])
-
-print(obj['items'][0]['searchInfo']['textSnippet'])
+# getting some diff info
+print(obj['items'][0]['searchInfo']['textSnippet']) # 0 is there for the list accessing the List[0], first item
