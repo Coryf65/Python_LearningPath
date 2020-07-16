4a9a59c32b2471a229417246c9ee70fb24735f7c
CoryF <coryf65@gmail.com>
2020-07-15 19:59:50 -0500

    now using labels and some props

--- 7 GUI with Tkinter/Ch02/05_hello_local.py
+++ 7 GUI with Tkinter/Ch02/05_hello_local.py
@@ -5,8 +5,13 @@
 from tkinter import *
 from tkinter import ttk
 
+# different Geometry Managers, .grid, .pack
+
+# common practice to write it as a class,
+# we could call this from another app
 class HelloApp:
 
+    # objects created in init
     def __init__(self, master):
 
         self.label = ttk.Label(master, text = "Hello, Tkinter!")
@@ -16,19 +21,19 @@ class HelloApp:
                    command = self.texas_hello).grid(row = 1, column = 0)
 
         ttk.Button(master, text = "Hawaii",
-                   command = self.hawaii_hello).grid(row = 1, column = 1)
+                   command = self.hawaii_hello).grid(row = 1, column = 1) # the command prop tells what to do when pressed
 
     def texas_hello(self):
-        self.label.config(text = 'Howdy, Tkinter!')
+        self.label.config(text = 'Howdy, Tkinter!') # changes the text
 
     def hawaii_hello(self):
         self.label.config(text = 'Aloha, Tkinter!')
 
-            
+# main loop            
 def main():            
     
     root = Tk()
-    app = HelloApp(root)
+    HelloApp(root)
     root.mainloop()
     
 if __name__ == "__main__": main()
--- 7 GUI with Tkinter/Ch03/01_label.py
+++ 7 GUI with Tkinter/Ch03/01_label.py
@@ -4,26 +4,30 @@
 
 from tkinter import *
 from tkinter import ttk        
-    
+import os
+
+cwd = os.path.dirname(os.path.abspath(__file__)) # path to this current files dir
+
 root = Tk()
 
 label = ttk.Label(root, text = "Hello, Tkinter!")
 label.pack()
 label.config(text = 'Howdy, Tkinter!')
 label.config(text = 'Howdy, Tkinter! It\'s been a really long time since we last met.  Great to see you again!')
-label.config(wraplength = 150)
+label.config(wraplength = 150) # num of pixels
 label.config(justify = CENTER)
 label.config(foreground = 'blue', background = 'yellow')
 label.config(font = ('Courier', 18, 'bold'))
 label.config(text = 'Howdy, Tkinter!')
 
-logo = PhotoImage(file = 'python_logo.gif') # change path to image as necessary
-label.config(image = logo)
+logo = PhotoImage(file = f'{cwd}/python_logo.gif') # change path to image as necessary
+label.config(image = logo) # does not save a reference to the image
 label.config(compound = 'text')
-label.config(compound = 'center')
-label.config(compound = 'left')
+label.config(compound = 'center') # displays text over the image
+label.config(compound = 'left') # image left of the text
 
+# always store the image, creating a img object in the label
 label.img = logo
-label.config(image = label.img)
+label.config(image = label.img) # tell label to use this image
 
 root.mainloop()
